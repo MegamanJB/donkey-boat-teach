54b23e7bcdbf86ff95a8ccbf33acd9694060cdee
MegamanJB <jmblachman@gmail.com>
2015-11-09 19:02:38 -0500

    ini

--- app/src/main/java/com/donkeyboatworks/teachmetorah/ActivityViewText.java
+++ app/src/main/java/com/donkeyboatworks/teachmetorah/ActivityViewText.java
@@ -3,40 +3,31 @@ package com.donkeyboatworks.teachmetorah;
 import android.os.Bundle;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.text.TextPaint;
 import android.text.method.ScrollingMovementMethod;
-import android.text.style.ClickableSpan;
 import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.http.TextHttpResponseHandler;
 
 import org.apache.http.Header;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class ActivityViewText extends ActionBarActivity implements View.OnClickListener {
+public class ActivityViewText extends ActionBarActivity {
 
     TextView textVerses;
-    Button buttonForward;
-    Button buttonBack;
-
     ListView translationListView;
     ArrayAdapter mArrayAdapter;
     ArrayList mTranslationList = new ArrayList();
@@ -78,13 +69,6 @@ public class ActivityViewText extends ActionBarActivity implements View.OnClickL
         translationListView.setAdapter(mArrayAdapter);
 
 
-//        buttonForward = (Button) findViewById(R.id.buttonForward);
-//        buttonForward.setOnClickListener(this);
-//
-//        buttonBack = (Button) findViewById(R.id.buttonBack);
-//        buttonBack.setOnClickListener(this);
-
-
         Bundle extras = this.getIntent().getExtras();
         Integer bookId = null;
         Integer chapterNum = 1;
@@ -111,13 +95,10 @@ public class ActivityViewText extends ActionBarActivity implements View.OnClickL
                 return true;
 
             case (MotionEvent.ACTION_UP) :
-                Log.d("onTouch","Action was UP");
+                Log.d("onTouch", "Action was UP");
                 int x = (int) event.getX();
                 int y = (int) event.getY();
-                String clickedWord = util_getWordAtPosition(x, y);
-                translate(clickedWord);
-
-                Log.d("onTouch", "clickedAroundStr: " + clickedWord);
+                handler_translateClick(x, y);
                 return true;
 
             case (MotionEvent.ACTION_CANCEL) :
@@ -157,40 +138,57 @@ public class ActivityViewText extends ActionBarActivity implements View.OnClickL
         return super.onOptionsItemSelected(item);
     }
 
-    private String util_getWordAtPosition(int x, int y)
-    {
-        int offset = textVerses.getOffsetForPosition(x, y);
-        int startRange = offset;
-        int endRange = offset+1;
-        String text = textVerses.getText().toString();
-
-        char curChar = text.charAt(startRange);
-        while (startRange > 0 && curChar != ' ') {
-            startRange--;
-            curChar = text.charAt(startRange);
-        }
-
-        curChar = text.charAt(endRange);
-        while (endRange < text.length()-1 && curChar != ' ') {
-            endRange++;
-            curChar = text.charAt(endRange);
-        }
-
-        String clickedWord = text.substring(startRange, endRange);
-        return clickedWord;
+    private void handler_translateClick(int x, int y) {
+        List<String> clickedWords = util_getWordsAtPosition(x, y);
+        display_translateWords(clickedWords);
     }
 
-    public Boolean isOnline() {
-        try {
-            Process p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
-            int returnVal = p1.waitFor();
-            boolean reachable = (returnVal==0);
-            return reachable;
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    private List<String> util_getWordsAtPosition(int x, int y)
+    {
+        List<String> translateWords = new ArrayList<>();
+
+        int offset = textVerses.getOffsetForPosition(x, y);
+        String fullText = textVerses.getText().toString();
+
+        Pair<Integer, Integer> clickedWordBounds = util_getWordBoundsAt(offset);
+        String clickedWord = fullText.substring(
+                clickedWordBounds.first, clickedWordBounds.second);
+
+        Pair<Integer, Integer> nextWordBounds = util_getWordBoundsAt(clickedWordBounds.second + 1);
+        String nextWord = fullText.substring(
+                nextWordBounds.first, nextWordBounds.second);
+
+        Pair<Integer, Integer> prevWordBounds = util_getWordBoundsAt(clickedWordBounds.first - 1);
+        String prevWord = fullText.substring(
+                prevWordBounds.first, prevWordBounds.second);
+
+        translateWords.add(clickedWord);
+        translateWords.add(prevWord + " " + clickedWord);
+        translateWords.add(clickedWord + " " + nextWord);
+        translateWords.add(prevWord + " " + clickedWord + " " + nextWord);
+
+        return translateWords;
+    }
+
+    private Pair<Integer, Integer> util_getWordBoundsAt(int offset)
+    {
+        int startRange = offset;
+        int endRange = offset + 1;
+        String fullText = textVerses.getText().toString();
+
+        char curChar = fullText.charAt(startRange);
+        while (startRange > 0 && curChar != ' ') {
+            startRange--;
+            curChar = fullText.charAt(startRange);
         }
-        return false;
+
+        curChar = fullText.charAt(endRange);
+        while (endRange < fullText.length()-1 && curChar != ' ') {
+            endRange++;
+            curChar = fullText.charAt(endRange);
+        }
+
+        return new Pair<>(startRange, endRange);
     }
 
     private void getVerses(int bookId, int chapterNum)
@@ -207,146 +205,15 @@ public class ActivityViewText extends ActionBarActivity implements View.OnClickL
         util_setTextViewText(displayText);
     }
 
-    private void makeRequest()
+    private void display_translateWords(List<String> clickedWords)
     {
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        params.put("key", "value");
-        params.put("more", "data");
-        client.get("http://www.google.com", params, new TextHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, String res) {
-                        // called when response HTTP status is "200 OK"
-                        Log.w("josh", "google success " + res);
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
-                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                        Log.w("josh", "fail");
-                    }
-                }
-        );
-    }
-
-    private void getTOC(final String searchString) {
-
-        Log.w("josh", searchString);
-
-        String response = versesDB.getVerseByUrl(searchString);
-        if (response != null) {
-            Log.w("josh", "using saved response");
-            textVerses.setText(response.substring(0, 200));
-            JSONArray responseArray = null;
-            try {
-                responseArray = new JSONArray(response);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            String toc = getTOCText(responseArray, 0);
-            util_setTextViewText(toc);
-            return;
+        for (String word : clickedWords)
+        {
+            request_translate(word);
         }
-
-        // Create a client to perform networking
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        params.put("key", "value");
-        params.put("more", "data");
-        client.get(searchString, params, new JsonHttpResponseHandler() {
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject jsonObject) {
-                Log.w("josh", "success jsonObject");
-                // displayTOC(jsonObject);
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.w("josh", "success JSONArray");
-                displayTOC(response, searchString);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                Log.w("josh", "fail jsonObject" + throwable.toString());
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-                Log.w("josh", "fail JSONArray");
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                Log.w("josh", "fail String");
-            }
-        });
     }
 
-    private void getVerses(String searchString) {
-
-        Toast.makeText(this, "setting query " + searchString, Toast.LENGTH_LONG).show();
-        Log.w("josh", searchString);
-
-
-//        String dbText = getFromNewDB();
-//        util_setTextViewText(dbText);
-        return;
-        /*
-
-        // Prepare your search string to be put in a URL
-        // It might have reserved characters or something
-        String urlString = "";
-//        try {
-//            urlString = URLEncoder.encode(searchString, "UTF-8");
-//        } catch (UnsupportedEncodingException e) {
-//
-//            // if this fails for some reason, let the user know why
-//            e.printStackTrace();
-//            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-//        }
-
-        // Create a client to perform networking
-        AsyncHttpClient client = new AsyncHttpClient();
-
-        // Have the client get a JSONArray of data
-        // and define how to respond
-        Log.w("josh", "sending " + searchString);
-
-//        String query = QUERY_URL + urlString;
-//        query = "http://openlibrary.org/search.json?q=test";
-//        query = "http://www.sefaria.org/api/texts/Kohelet.5";
-
-        client.get(searchString,
-                new JsonHttpResponseHandler() {
-
-                    @Override
-                    public void onSuccess(int statusCode,
-                                          Header[] headers,
-                                          JSONObject response) {
-                        Log.w("josh", "success json object");
-                        displayVerse(response);
-                    }
-
-                    @Override
-                    public void onSuccess(int statusCode,
-                                          Header[] headers,
-                                          JSONArray response) {
-                        Log.w("josh", "success got json array");
-                        //displayVerse(response);
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String errStr, Throwable throwable) {
-                        Log.w("josh", "fail");
-                    }
-                });
-
-                */
-    }
-
-    private void translate(final String word)
+    private void request_translate(final String word)
     {
        // Illegal character in query at index 47: http://api.mymemory.translated.net/get?q=%20?':</i>
 
@@ -389,28 +256,6 @@ public class ActivityViewText extends ActionBarActivity implements View.OnClickL
                 });
     }
 
-    private void displayVerse(JSONObject jsonObject)
-    {
-        String title = jsonObject.optString("heRef");
-        String english = jsonObject.optString("text");
-        String hebrew = jsonObject.optString("he");
-        nextPage = jsonObject.optString("next");
-        prevPage = jsonObject.optString("prev");
-
-        util_setTextViewText(title + "\n\n\n" + hebrew);
-//        textVerses.setText(title + "\n\n\n" + hebrew);
-    }
-
-    private void displayTOC(JSONArray response, String url)
-    {
-        String responseStr = response.toString();
-        versesDB.insertVerseByUrl(url, responseStr);
-
-        // Log.w("josh",response.toString());
-        String toc = getTOCText(response, 0);
-        textVerses.setText(toc);
-    }
-
     private void displayTranslation(String word, JSONObject jsonObject)
     {
         JSONObject responseData = jsonObject.optJSONObject("responseData");
@@ -426,141 +271,9 @@ public class ActivityViewText extends ActionBarActivity implements View.OnClickL
         //Toast.makeText(this, displayText, Toast.LENGTH_SHORT).show();
     }
 
-    private String getTOCText(JSONArray catContents, int level)
-    {
-        String toc = "";
-        JSONObject category = null;
-        try {
-            int TEST_MAX = 2;
-
-            for (int i = 0; i < catContents.length() && i < TEST_MAX; i++) {
-                for (int j = 0; j < level; j++) {
-                    toc += "--";
-                }
-
-                JSONObject content = (JSONObject) catContents.get(i);
-                String catName = content.optString("category");
-                if (catName != "null" && catName != null && catName.trim() != "") {
-                    String heCatName = content.optString(("heCategory"));
-                    JSONArray contents = content.optJSONArray("contents");
-
-                    toc += (i+1) + ") " + catName + "\n";
-                    if (contents != null && contents.length() != 0) {
-                        toc += getTOCText(contents, level + 1);
-                    }
-                }
-                else {
-                    String title = content.optString("title");
-                    if (title != "null") {
-                        toc += (i+1) + ". " + title;
-                    }
-                }
-
-                toc += "\n";
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return toc;
-    }
-
     private void util_setTextViewText(final String textViewStr)
     {
         textVerses.setText(textViewStr);
-
-        //String definition = "Clickable words in text view ".trim();
-        //TextView definitionView = (TextView) findViewById(R.id.text);
-
-//        textVerses.setMovementMethod(LinkMovementMethod.getInstance());
-//        textVerses.setText(textViewStr, TextView.BufferType.SPANNABLE);
-//        Spannable spans = (Spannable) textVerses.getText();
-//        BreakIterator iterator = BreakIterator.getWordInstance(Locale.US);
-//        iterator.setText(textViewStr);
-//        int start = iterator.first();
-//        for (int end = iterator.next(); end != BreakIterator.DONE; start = end, end = iterator
-//                .next()) {
-//            String possibleWord = textViewStr.substring(start, end);
-//            if (Character.isLetterOrDigit(possibleWord.charAt(0))) {
-//                ClickableSpan clickSpan = ClickableSpan_translate(possibleWord);
-//                spans.setSpan(clickSpan, start, end,
-//                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//            }
-//        }
     }
 
-    private ClickableSpan ClickableSpan_translate(final String word) {
-        return new ClickableSpan() {
-            final String mWord;
-            {
-                mWord = word;
-            }
-
-            @Override
-            public void onClick(View widget) {
-                Log.d("tapped on:", mWord);
-                Toast.makeText(widget.getContext(), mWord, Toast.LENGTH_SHORT).show();
-                translate(mWord);
-                //String request = "https://www.googleapis.com/language/translate/v2?source=en&target=de&q=Hello%20world&key=AIzaSyC13zwX4iI3LpAdVvmnjLQo4IqdMaI7gd4";
-                //String request = SEFARIA_URL + TEXTS_URI + mWord + "." + currentPage;
-                //getVerses(request);
-            }//keytool -exportcert -alias androiddebugkey -keystore path-to-debug-or-production-keystore -list -v
-
-            public void updateDrawState(TextPaint ds) {
-                super.updateDrawState(ds);
-            }
-        };
-    }
-
-    private ClickableSpan getClickableSpan(final String word) {
-        return new ClickableSpan() {
-            final String mWord;
-            {
-                mWord = word;
-            }
-
-            @Override
-            public void onClick(View widget) {
-                Log.d("tapped on:", mWord);
-                Toast.makeText(widget.getContext(), mWord, Toast.LENGTH_SHORT).show();
-                String request = SEFARIA_URL + TEXTS_URI + mWord + "." + currentPage;
-                getVerses(request);
-            }
-
-            public void updateDrawState(TextPaint ds) {
-                super.updateDrawState(ds);
-            }
-        };
-    }
-
-    public void loadNextPage()
-    {
-        if (nextPage != "null") {
-            String request = SEFARIA_URL + TEXTS_URI + nextPage;
-            getVerses(request);
-        }
-    }
-
-    public void loadPrevPage()
-    {
-        if (prevPage != "null") {
-            String request = SEFARIA_URL + TEXTS_URI + prevPage;
-            getVerses(request);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        switch(v.getId()) {
-            case R.id.buttonBack:
-                loadPrevPage();
-                break;
-
-            case R.id.buttonForward:
-                loadNextPage();
-                break;
-        }
-    }
 }
